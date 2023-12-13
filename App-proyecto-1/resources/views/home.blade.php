6e178769fe33da4859eadd0098e28037cc34d058
Lmejia0093 <davidmejia0093@gmail,com>
2023-12-13 14:55:28 -0600

    agregando tabla a page2

--- App-proyecto-1/resources/views/home.blade.php
+++ App-proyecto-1/resources/views/home.blade.php
@@ -17,6 +17,9 @@
                 <th>Orden</th>
                 <th>Nombre</th>
                 <th>Cantidad</th>
+                <th>Numero Req</th>
+                <th>Estado</th>
+                <th>Accion</th>
             </tr>
         </thead>
         <tbody class="table-group-divider">
@@ -26,7 +29,55 @@
                 <td scope="row">1</td>
                 <td>Red</td>
                 <td>2</td>
+                <td>20001</td>
+                <td>Pendiente</td>
+                <td>
+                    <a class="btn btn-warning" href="http://">Editar</a>
+                    <a class="btn btn-danger" href="http://">Eliminar</a>
+                </td>
             </tr>
+
+            <tr
+            class="table"
+        >
+            <td scope="row">1</td>
+            <td>Red</td>
+            <td>2</td>
+            <td>20001</td>
+            <td>Pendiente</td>
+            <td>
+                <a class="btn btn-warning" href="http://">Editar</a>
+                <a class="btn btn-danger" href="http://">Eliminar</a>
+            </td>
+        </tr>
+
+        <tr
+        class="table"
+    >
+        <td scope="row">1</td>
+        <td>Red</td>
+        <td>2</td>
+        <td>20001</td>
+        <td>Pendiente</td>
+        <td>
+            <a class="btn btn-warning" href="http://">Editar</a>
+            <a class="btn btn-danger" href="http://">Eliminar</a>
+        </td>
+    </tr>
+
+    <tr
+    class="table"
+>
+    <td scope="row">1</td>
+    <td>Red</td>
+    <td>2</td>
+    <td>20001</td>
+    <td>Pendiente</td>
+    <td>
+        <a class="btn btn-warning" href="http://">Editar</a>
+        <a class="btn btn-danger" href="http://">Eliminar</a>
+    </td>
+</tr>
           
         </tbody>
         <tfoot>
